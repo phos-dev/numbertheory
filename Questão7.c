c1ec54175c119b6a1618db84f91db55aa4ea3bcd
Pedro Henrique <38169759+phos-dev@users.noreply.github.com>
2020-11-26 20:36:18 -0300

    Add files via upload

---
+++ Questão7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a, m, b;
+    scanf("%d%d", &a, &m);
+    for (int i = 0; i < m; i++){
+        if ((a * i) % m == 1){b = i; break;}
+    }
+    printf("%d", b);
+  	return 0;
+}
