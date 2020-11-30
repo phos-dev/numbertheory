c6bcaa2911059615ee0bddc4785e7be0d97aaf2f
Pedro Henrique <38169759+phos-dev@users.noreply.github.com>
2020-11-30 19:44:24 -0300

    Update Questão6.py

--- Questão6.py
+++ Questão6.py
@@ -1,4 +1,5 @@
 # Escrever um programa para encontrar os coeficientes s e t da combinação linear mdc(a, b) = s · a + t · b.
+# Formato do input: a b
 # Para rodar este código é necessário ter instalada a ide pycharm.
 
 def mdc_ite(a,b):
@@ -11,11 +12,11 @@ def mdc_ite(a,b):
     return b
 
 
-a = 14
-b = 6
+a, b = list(map(int, input().split()))
+if(a < b): b, a = a, b
 s = (a // b) - (a // b) - (a // b)
 t = 1
-print("O mdc({:d},{:d}) = {:d}".format(a,b,mdc_ite(a,b)))
-print("logo {:d} = {:d} . {:d} + {:d} . {:d}".format(mdc_ite(a,b),s,a,t,b))
+#print("O mdc({:d},{:d}) = {:d}".format(a,b,mdc_ite(a,b)))
+print("{:d} = {:d}*{:d} + {:d}*{:d}".format(mdc_ite(a,b),s,a,t,b))
 
-print("Então s é igual a {:d} e t é igual a {:d}" .format(s,t))
+#print("Então s é igual a {:d} e t é igual a {:d}" .format(s,t))
